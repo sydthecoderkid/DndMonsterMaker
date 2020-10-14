432e27e0931ce4a8b272807e701d82a96eeaf1fc
sydthecoderkid <bookmasterdc@gmail.com>
2020-10-14 00:46:23 -0400

    Finished Main Menu

--- Code/src/CreateUI.java
+++ Code/src/CreateUI.java
@@ -1,15 +1,24 @@
 import javax.swing.*;
+import java.awt.*;
 
 public class CreateUI {
 
     private JPanel DndMonsterMaker;
     private JLabel DnDMonsterMaker;
+    private JButton createNewMonsterButton;
+    private JButton loadMonsterButton;
 
     public void createWindow() {
         JFrame frame = new JFrame("DndMonsterMaker");
         frame.setContentPane(new CreateUI().DndMonsterMaker);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        Dimension dimension = new Dimension(1000,700);
+        frame.setPreferredSize(dimension);
         frame.pack();
         frame.setVisible(true);
+        frame.setLocationRelativeTo(null);
     }
+
+
+
 }
