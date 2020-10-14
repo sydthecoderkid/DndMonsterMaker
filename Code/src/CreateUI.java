9ce51b3e0614980a5ed30097df7252c2d394453e
sydthecoderkid <bookmasterdc@gmail.com>
2020-10-14 00:18:28 -0400

    Working on GUI

--- Code/src/CreateUI.java
+++ Code/src/CreateUI.java
@@ -1,43 +1,15 @@
-import java.awt.*;
 import javax.swing.*;
 
+public class CreateUI {
 
+    private JPanel DndMonsterMaker;
+    private JLabel DnDMonsterMaker;
 
-
-public class CreateUI extends JPanel{
-    static JFrame frame = new JFrame("D&DMonsterMaker");
-
-    public static void createWindow(){
-        frame.getContentPane().add(new CreateUI());
-        GroupLayout layout = new GroupLayout(frame);
-        int width = 1000;
-        int height = 700;
-        setTitle();
-        createButtons();
+    public void createWindow() {
+        JFrame frame = new JFrame("DndMonsterMaker");
+        frame.setContentPane(new CreateUI().DndMonsterMaker);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        JFrame.setDefaultLookAndFeelDecorated(true);
         frame.pack();
-        frame.setSize (width, height);
-        frame.setLocationRelativeTo(null);
         frame.setVisible(true);
-
     }
-
-    public void paint(Graphics g){
-
-    }
-
-    public static void setTitle(){
-        JLabel label = new JLabel();
-        label.setFont(new Font("TimesRoman", Font.PLAIN, 30));
-        label.setHorizontalAlignment(0);
-        label.setVerticalAlignment(1);
-        label.setText("D&D Monster Maker");
-        frame.add(label);
-    }
-
-    public static void createButtons(){
-
-    }
-
 }
--- Code/src/Main.java
+++ Code/src/Main.java
@@ -1,7 +1,8 @@
 public class Main {
 
     public static void main(String[] args){
-        CreateUI.createWindow();
+        CreateUI creator = new CreateUI();
+        creator.createWindow();
 
 
     }
