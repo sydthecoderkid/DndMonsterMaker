4c7a1183b7c7be0bc30d9b8ef4e1a964cc6b72c5
sydthecoderkid <bookmasterdc@gmail.com>
2020-10-19 18:07:41 -0400

    Finished text and label layout

--- Code/src/CreateMonsters.java
+++ Code/src/CreateMonsters.java
@@ -16,36 +16,22 @@ public class CreateMonsters implements ActionListener {
 
     TextFieldActionListener textFieldListener;
 
-
     public CreateMonsters(){
-        textFieldListener = new TextFieldActionListener();
-        AC.addActionListener(textFieldListener);
-        AC.addFocusListener(new FocusListener() {
-            public void focusGained(FocusEvent e) {
-                AC.setText("");
-            }
 
-            public void focusLost(FocusEvent e) {
-                // nothing
-            }
-        });
+    AC = AssignListener(AC);
+    monsterNameTextField = AssignListener(monsterNameTextField);
+    HPTextField = AssignListener(HPTextField);
+    setDefaultNames("AC", AC);
+    setDefaultNames("Monster Name", monsterNameTextField);
+    setDefaultNames("HPTextField", HPTextField);
 
-        monsterNameTextField.addActionListener(textFieldListener);
-        monsterNameTextField.addFocusListener(new FocusListener() {
-            public void focusGained(FocusEvent e) {
-                monsterNameTextField.setText("");
-            }
 
-            public void focusLost(FocusEvent e) {
-                // nothing
-            }
-        });
+
     }
 
     public void actionPerformed(ActionEvent e) { //Called when the button is pressed.
         setMonsterUI();
         monsterinfo = new MonsterInfo();
-
     }
 
 
@@ -53,10 +39,34 @@ public class CreateMonsters implements ActionListener {
 
     public void setMonsterUI() {
 
-
+        //Initalizes the content pane.
         CreateUI.frame.setContentPane(new CreateMonsters().MonsterPanel);
         CreateUI.frame.revalidate();
 
+    }
+
+
+    public JTextField AssignListener(JTextField caller){
+        textFieldListener = new TextFieldActionListener();
+        caller.addActionListener(textFieldListener);
+        caller.addFocusListener(new FocusListener() {
+            public void focusGained(FocusEvent e) {
+                if(caller.getText().equals("HP") || caller.getText().equals("AC") || caller.getText().equals("Monster Name")){
+                    caller.setText(""); //Checks to see if the current text is the default, and if so, erases it.
+                }
+
+            }
+
+            public void focusLost(FocusEvent e) {
+                // nothing
+            }
+        });
+
+        return caller;
+    }
+
+    public static void setDefaultNames(String name, JTextField textField){
+        textField.setName(name); //Sets the desired text field's name to parameter
     }
 
 
