41179cff8f37aca7ab5a54722c7dca19ae8e6abb
sydthecoderkid <bookmasterdc@gmail.com>
2020-10-23 22:25:07 -0400

    Editing UI

--- Code/src/CreateStatsUI.java
+++ Code/src/CreateStatsUI.java
@@ -5,6 +5,10 @@ import java.awt.event.ActionListener;
 public class CreateStatsUI implements ActionListener {
     private JPanel StatsPanel;
     private JLabel Stats;
+    private JTextField strBonusTextField;
+    private JTextField dexBonusTextField;
+    private JTextField charBonusTextField;
+    private JTextField wisBonusTextField;
 
     @Override
     public void actionPerformed(ActionEvent e) {
@@ -15,5 +19,10 @@ public class CreateStatsUI implements ActionListener {
         CreateUI.frame.setContentPane(new CreateStatsUI().StatsPanel);
         CreateUI.frame.revalidate();
     }
+
+    public static void setDefaultNames(String name, JTextField textField){
+        textField.setName(name); //Sets the desired text field's name to parameter
+    }
+
 
 }
